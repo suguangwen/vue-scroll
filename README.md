0677fe774203cc918efe4b714953eb7e454ce709
suguangwen <564682916@qq.com>
2016-04-12 16:06:20 +0800

    updata

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # vue-rolling
+vue infinite scroll which can work.
+
+
+借鉴于ElemeFE/vue-infinite-scroll．
+重新设计了核心算法．
+将持续更新和添加与滚动有关的效果
+
+
+＠TODO 增加环境与测试用例．
+
+# 使用方法//Usage
+
+Use v-infinite-scroll to enable the infinite scroll, and use infinite-scroll-* attributes to define its options.
+
+使用v-infinite-scroll进行无限滚动翻页,使用 infinite-scroll- * 属性来定义它的选项。
+
+```HTML
+<div v-infinite-scroll="loadMore()" infinite-scroll-foot="500">
+
+</div>
+```
+
+```JavaScript
+  var app = document.querySelector('.app')
+  new Vue({
+    el: 'body',
+    data: function () {
+      return {busy: false, a: [], b: []}
+    },
+    methods: {
+      loadMore: function () {
+         //当滚动条距离底部高度等于你在infinite-scroll-foot设置的高度时将运行一次此函数
+         //if scrollTop = infinite-scroll-foot , function run.
+        this.busy = true
+
+        for (var i = this.a.length; i < this.b.length; i++) {
+          this.a.push(this.b[i])
+          if (i % 6 === 5) {
+            break
+          }
+        }
+        this.busy = false
+      }
+    },
+    ready: function () {
+      //无限滚动翻页实例（请按照自己的需要去修改）
+      //infinite-scroll the instance.
+      $.ajax({
+          url: '#',
+          type: 'get',
+          success: function (res) {
+            if (res.status === 200) {
+              // 初始化数据 先获取所有数据并初始化前６条
+              //Initialize the data,get 0-6 data of the array.
+              this.$set('b', res.data)
+              for (var i = 0; i < 6; i++) {
+                this.a.push(this.b[i])
+              }
+            }
+          }
+        })
+    }
+  })
+```
+
+# 选项//Options
+
+| infinite-scroll/Option | Description |
+| ----- | ----- |
+| infinite-scroll-foot | 设定滚动条距离底部的高度．// Set the height of the scroll bar at the bottom of the distance. |
+
+# License
+
+MIT
---
+++ src/vue-rolling.js
@@ -0,0 +1,185 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) : (factory((global.infiniteScroll = global.infiniteScroll || {})))
+}(this, function (exports) {
+  'use strict'
+  var throttle = function throttle (fn, delay) {
+    var now, lastExec, timer, context, args //eslint-disable-line
+
+    var execute = function execute () {
+      fn.apply(context, args)
+      lastExec = now
+    }
+
+    return function () {
+      context = this
+      args = arguments
+
+      now = Date.now()
+
+      if (timer) {
+        clearTimeout(timer)
+        timer = null
+      }
+
+      if (lastExec) {
+        var diff = delay - (now - lastExec)
+        if (diff < 0) {
+          execute()
+        } else {
+          timer = setTimeout(function () {
+            execute()
+          }, diff)
+        }
+      } else {
+        execute()
+      }
+    }
+  }
+
+  var getScrollTop = function getScrollTop () {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+    return scrollTop
+  }
+
+  var getComputedStyle = document.defaultView.getComputedStyle
+
+  var getScrollEventTarget = function getScrollEventTarget (element) {
+    var currentNode = element
+    while (currentNode && currentNode.tagName !== 'HTML' && currentNode.nodeType === 1) {
+      var overflowY = getComputedStyle(currentNode).overflowY
+      if (overflowY === 'scroll' || overflowY === 'auto') {
+        return currentNode
+      }
+      currentNode = currentNode.parentNode
+    }
+    return window
+  }
+
+  var getVisibleHeight = function getVisibleHeight () {
+    return document.documentElement.clientHeight
+  }
+
+  var getScrollHeight = function getScrollHeight () {
+    return document.documentElement.scrollHeight
+  }
+
+  var isAttached = function isAttached (element) {
+    var currentNode = element.parentNode
+    while (currentNode) {
+      if (currentNode.tagName === 'HTML') {
+        return true
+      }
+      if (currentNode.nodeType === 11) {
+        return false
+      }
+      currentNode = currentNode.parentNode
+    }
+    return false
+  }
+
+  var infiniteScroll = {
+    doBind: function doBind () {
+      if (this.binded) return // eslint-disable-line
+      this.binded = true
+
+      var directive = this
+      var element = directive.el
+
+      directive.scrollEventTarget = getScrollEventTarget(element)
+      directive.scrollListener = throttle(directive.doCheck.bind(directive), 200)
+      directive.scrollEventTarget.addEventListener('scroll', directive.scrollListener)
+
+      var viewportFootExpr = element.getAttribute('infinite-scroll-foot')
+      var viewportFoot = 0
+      if (viewportFootExpr) {
+        viewportFoot = Number(directive.vm.$get(viewportFootExpr))
+        if (isNaN(viewportFoot)) {
+          viewportFoot = 0
+        }
+      }
+      directive.viewportFoot = viewportFoot
+
+      var viewportWaterfallExpr = element.getAttribute('infinite-scroll-waterfall')
+      var Waterfall = false
+      if (viewportWaterfallExpr) {
+        Waterfall = Boolean(directive.vm.$get(viewportWaterfallExpr))
+      }
+      directive.Waterfall = Waterfall
+
+      var immediateCheckExpr = element.getAttribute('infinite-scroll-immediate-check')
+      var immediateCheck = true
+      if (immediateCheckExpr) {
+        immediateCheck = Boolean(directive.vm.$get(immediateCheckExpr))
+      }
+      directive.immediateCheck = immediateCheck
+
+      if (immediateCheck) {
+        directive.doCheck()
+      }
+
+    },
+
+    doCheck: function doCheck (force) {
+      var scrollEventTarget = this.scrollEventTarget
+      var element = this.el
+      var viewportFoot = this.viewportFoot
+      var viewportWaterfall = this.Waterfall
+      if (force !== true && this.disabled) return //eslint-disable-line
+      var shouldTrigger = false
+      if (scrollEventTarget === element) {
+        shouldTrigger = getVisibleHeight() + getScrollTop() + viewportFoot <= getScrollHeight()
+      } else {
+        shouldTrigger = getVisibleHeight() + getScrollTop() + viewportFoot >= getScrollHeight()
+      }
+
+      if (shouldTrigger && this.expression) {
+        this.vm.$get(this.expression)
+      }
+
+      if(viewportWaterfall) {
+      }
+    },
+
+    bind: function bind () {
+      var directive = this
+      var element = this.el
+
+      directive.vm.$on('hook:ready', function () {
+        if (isAttached(element)) {
+          directive.doBind()
+        }
+      })
+
+      this.bindTryCount = 0
+
+      var tryBind = function tryBind () {
+        if (directive.bindTryCount > 10) return //eslint-disable-line
+        directive.bindTryCount++
+        if (isAttached(element)) {
+          directive.doBind()
+        } else {
+          setTimeout(tryBind, 50)
+        }
+      }
+
+      tryBind()
+    },
+
+    unbind: function unbind () {
+      this.scrollEventTarget.removeEventListener('scroll', this.scrollListener)
+    }
+  }
+
+  if (window.Vue) {
+    window.infiniteScroll = infiniteScroll
+    Vue.use(install)
+  }
+
+  function install (Vue) {
+    Vue.directive('infiniteScroll', infiniteScroll)
+  }
+
+  exports.install = install
+  exports.infiniteScroll = infiniteScroll
+
+}))
